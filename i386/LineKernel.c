b21ad5e0964a347d89558ef7183c2f0250ba86bd
NexusSfan <nexussfan@duck.com>
2026-03-24 15:03:36 -0700

    Revert "Propper ANSI support"
    This reverts commit baeaf22eb1d34ae40206aa41a8bbf635742252dd.

--- i386/LineKernel.c
+++ i386/LineKernel.c
@@ -11,18 +11,8 @@
 void kernel_main(void)
 {
 	/* This is a placeholder */
-	terminal_log("Starting Terminal Initialization", "INFO");
 	terminal_initialize();
-	terminal_log("Terminal Initialized", "OK");
+	terminal_writestring("Initializing Terminal...\n");
 
-	terminal_log("Starting Kernel", "INFO");
-	
-	// Test ANSI escape sequences
-	terminal_writestring("\x1b[32mHello, LineKernel in green!\x1b[0m\n");
-	terminal_writestring("\x1b[31mError message in red!\x1b[0m\n");
-	terminal_writestring("\x1b[2J\x1b[H"); // Clear screen and move to home
-	terminal_writestring("\x1b[10;20H"); // Move cursor to row 10, col 20
-	terminal_writestring("Cursor positioned here!\n");
-	
-	terminal_log("ANSI Support Test Complete", "OK");
+	terminal_writestring("Hello, LineKernel!\n");
 }
