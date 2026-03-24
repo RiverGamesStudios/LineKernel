1d064fe823ca19daeeab58f8e92ebd74374b9ce5
colebohte <colebohte@rivergames.ca>
2026-03-23 22:50:02 -0400

    colebohte's brain shart

--- i386/config.h
+++ i386/config.h
@@ -1,1 +1,8 @@
-../config.h
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+/* Default background colors for LineRenderer */
+#define LineRenderer_DEFAULTFOREGROUND VGA_COLOR_LIGHT_GREY
+#define LineRenderer_DEFAULTBACKGROUND VGA_COLOR_BLACK
+
+#endif
