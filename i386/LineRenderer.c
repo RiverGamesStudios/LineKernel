ce238d23fa2aa85cb5e321ab037f0b82c663a272
NexusSfan <nexussfan@duck.com>
2026-04-04 07:54:02 -0700

    Fix backspace in serial console

--- i386/LineRenderer.c
+++ i386/LineRenderer.c
@@ -129,6 +129,20 @@ void terminal_backspace(void)
 	write_serial('\b');
 	write_serial(' ');
 	write_serial('\b');
+	if (terminal_column == -1) {
+		// ^[1A - Move cursor one line up
+		write_serial('\033');
+		write_serial('[');
+		write_serial('1');
+		write_serial('A');
+		// ^[999C - Move cursor 999 characters right
+		write_serial('\033');
+		write_serial('[');
+		write_serial('9');
+		write_serial('9');
+		write_serial('9');
+		write_serial('C');
+	}
 #endif // serial_console
 }
 
