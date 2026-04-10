06a21a993423a20dca1c205b547e79d3f60950c1
NexusSfan <nexussfan@duck.com>
2026-04-10 11:48:57 -0700

    Remove unused typedefs

--- i386/bios_tools.h
+++ i386/bios_tools.h
@@ -7,16 +7,4 @@ void outb(uint16_t port, uint8_t val);
 void io_wait(void);
 void insl(int port, uint32_t addr, int cnt);
 
-#ifndef UInt32
-typedef uint32_t UInt32;
-#endif //! UInt32
-
-#ifndef UCHAR
-typedef unsigned char UCHAR;
-#endif //! UCHAR
-
-#ifndef USHORT
-typedef unsigned short USHORT;
-#endif //! USHORT
-
 #endif
