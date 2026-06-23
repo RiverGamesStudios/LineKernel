f9923fb7f40536527bf65894e4f96189c50c40de
NexusSfan <nexussfan@duck.com>
2026-06-23 10:28:56 -0700

    Fix some indentation

--- x86/dma.h
+++ x86/dma.h
@@ -6,8 +6,8 @@
 
 #include <stdint.h>
 
-#define DMA_MODE_READ       0x46	/* Read from disk (transfers memory -> FDC write) */
-#define DMA_MODE_WRITE      0x4A	/* Write to disk (transfers FDC read -> memory) */
+#define DMA_MODE_READ		0x46	/* Read from disk (transfers memory -> FDC write) */
+#define DMA_MODE_WRITE		0x4A	/* Write to disk (transfers FDC read -> memory) */
 
 void floppy_dma_init(uint8_t* buffer, uint32_t length, uint8_t mode);
 
